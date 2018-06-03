0a6d9dba7587e4a1dcc2c09ce70e1b1cc4a5a9f5
Mia Chen <ya.ting.chen110@gmail.com>
2018-06-03 19:14:26 +0800

    fix login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -56,6 +56,7 @@ class AuthController extends Controller
             'tel'=> $request->tel,
            'img' => $request->img,
        ]);
+       return response()->json(['success' => true]);
     }
 
     public function login(Request $request)
