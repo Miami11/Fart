0cdb03e8d74dfd7c06197ad086b19db9b0e690d2
Joe <>
2018-07-01 16:00:41 +0800

    layout

--- resources/views/product/component/login.blade.php
+++ resources/views/product/component/login.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/cssReset.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/login.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/modal.css') }}">
         <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
         <title>F.A.R.T</title>
         <script src="{{ asset('js/vue.js') }}"></script>
@@ -19,41 +20,83 @@
             <div v-if="operation == 1">
                 <div class="loginId">
                     帳號：
-                    <input type="text">
+                    <input type="text" v-model="loginEmail">
                 </div>
-                <div class="loginPwd">
+                <div class="loginPwd por">
                     密碼：
-                    <input type="password">
+                    <input :type="loginPwdType" v-model="loginPwd" maxlength="10">
+                    <div class="pwdSwitch" v-on:click="showLoginPwd = true" v-show="showLoginPwd == false">
+                        <i class="fas fa-eye"></i>
+                    </div>
+                    <div class="pwdSwitch" v-on:click="showLoginPwd = false" v-show="showLoginPwd == true">
+                        <i class="fas fa-eye-slash"></i>
+                    </div>
                 </div>
-                <button>登入</button>
+                <button v-on:click="login">登入</button>
             </div>
             <div v-if="operation == 2">
                 <div class="loginId">
                     帳號：
-                    <input type="text" placeholder="請使用有效信箱作為登入帳號">
+                    <input type="text" placeholder="請使用信箱作為登入帳號" v-model="regisEmail">
                 </div>
-                <div class="loginPwd">
+                <div class="loginId">
+                    姓名：
+                    <input type="text" v-model="regisName">
+                </div>
+                <div class="loginId">
+                    電話：
+                    <input type="tel" v-model="regisTel">
+                </div>
+                <div class="loginPwd por">
                     密碼：
-                    <input type="password">
+                    <input :type="RegisPwdType" v-model="regisPwd" maxlength="10">
+                    <div class="pwdSwitch" v-on:click="showRegisPwd = true" v-show="showRegisPwd == false">
+                        <i class="fas fa-eye"></i>
+                    </div>
+                    <div class="pwdSwitch" v-on:click="showRegisPwd = false" v-show="showRegisPwd == true">
+                        <i class="fas fa-eye-slash"></i>
+                    </div>
                 </div>
-                <button>註冊</button>
+                <button v-on:click="register">註冊</button>
             </div>
         </div>
-        <!-- <p>LOGIN</p>
-        <input type="text" v-model="email">   
-        <input type="password" v-model="password">
-        <h1 @click="login">BTN</h1> -->
+        
+        <!-- <div class="modal message">
+            <div class="message-body">
+                <div class="close-modal"></div>
+                <p class="message-content">
+                    gg
+                </p>
+                <button class="message-confirm">OK</button>
+            </div>
+        </div> -->
+
     </div>
 </body>
 </html>
 
+<script defer src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
 <script>
 new Vue({
     el: '#app',
     data: {
-        email: '',
-        password: '',
+        loginEmail: '',
+        loginPwd: '',
         operation: 1,
+        regisEmail: '',
+        regisPwd: '',
+        regisName: '',
+        regisTel: '',
+        showLoginPwd: false,
+        showRegisPwd: false
+    },
+    computed: {
+        loginPwdType: function(){
+            return this.showLoginPwd === false? 'password' : 'text';
+        },
+        RegisPwdType: function(){
+            return this.showRegisPwd === false? 'password' : 'text';
+        }
     },
     mounted: function(){
         
@@ -61,17 +104,43 @@ new Vue({
     methods: {
     	login(){
             $.ajax({
-                url: 'http://fart.test/api/auth/login',
+                url: '../api/auth/login',
                 type: 'post',
                 data: {
-                    'email': this.email,
-                    'password': this.password,
+                    'email': this.loginEmail,
+                    'password': this.loginPwd,
                 },
                 success(data){
                     console.log(data);
+                    if (data.success) {
+                        alert('login success');
+                        localStorage.setItem('jwt_token', data.result.token);
+                        location.href = '../../';
+                    }else{
+                        alert(data.message);
+                    }
                 }
             });
     	},
+        register(){
+            $.ajax({
+                url: '../api/register',
+                type: 'post',
+                data: {
+                    'name': this.regisName,
+                    'email': this.regisEmail,
+                    'password': this.regisPwd,
+                    'tel': this.regisTel,
+                    'img': 'uploads/images/members/member1.jpg'
+                },
+                success(data){
+                    console.log(data);
+                    if (data.status == 'success') {
+                        alert('oh ya!');
+                    }
+                }
+            });
+        },
     }
 })
 </script>
