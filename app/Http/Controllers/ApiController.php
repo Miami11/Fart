3faba7ac017b16a6a98eea0b4e5682b9dd0c209c
Mia Chen <ya.ting.chen110@gmail.com>
2018-07-02 11:39:25 +0800

    [修改]驗證格式

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -25,13 +25,17 @@ class ApiController extends Controller
             'name' => 'required|max:10',
             'email' => 'required|email|max:100|unique:users',
             'password' => 'required|alphaNum|min:5|max:10',
-            'tel' => 'required|numeric|min:10|max:10',
+            'tel' => 'required|numeric|regex:/(09)[0-9]{8}/',
         ];
         $messages = [
-            'name'    => '請務必填寫,最大10個字數',
-            'email'    => '最大字數100且只能註冊一次',
+            'name.required'    => '請務必填寫',
+            'name.max'    => '最大10個字數',
+            'email.email'    => '請輸入有效的email帳號',
+            'email.unique' => '此帳號已存在',
+            'email.max' => 'email字數請低於100',
             'password' => '5~10個密碼需含英文數字',
-            'tel'      => '10位數字',
+            'tel.regex'      => '10位數字',
+            'tel.numeric'      => '需要是數字',
         ];
         $validator = Validator::make($credentials, $rules,$messages);
         if ($validator->fails()) {
@@ -62,8 +66,11 @@ class ApiController extends Controller
             'password' => 'required|alphaNum|min:5|max:10',
         ];
         $messages = [
-            'email'    => '請輸入eamil格式',
-            'password' => '5~10個密碼需含英文數字',
+            'email.email'    => '請輸入eamil格式',
+            'email.max'    => 'email最多100個字數',
+            'password.alphaNum' => '5~10個密碼需含英文數字',
+            'password.min' => '至少5個含英文數字密碼',
+            'password.max' => '最多１０個含英文數字密碼',
         ];
         $validator = Validator::make($credentials, $rules,$messages);
         if ($validator->fails()) {
